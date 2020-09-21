7a51bbb41faa4299ff2a51cd9978368f955117a5
Leo1478 <45522641+Leo1478@users.noreply.github.com>
2020-09-21 15:40:47 -0400

    small changes

--- Assets/scripts/MoveCharacter.cs
+++ Assets/scripts/MoveCharacter.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class MoveCharacter : MonoBehaviour
-{
+public class MoveCharacter : MonoBehaviour{
     public float moveSpeed = 5f;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Jump();
+    void Update(){
+        jump();
+        move();
+    }
+
+    void move(){
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
     }
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump")) {
+
+    void jump(){
+        if (Input.GetButtonDown("Jump")){
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }   
     }
